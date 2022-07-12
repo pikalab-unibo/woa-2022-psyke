363f2d55cc8784d0da6ff2d9ab620e5fa6563514
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2022-07-12 16:57:31 +0200

    chore: improve snippets in sec 4

--- listings/iris-rules-cart.pl
+++ listings/iris-rules-cart.pl
@@ -1,5 +1,3 @@
-iris(SepalLength, SepalWidth, PetalLength, PetalWidth, setosa) :-
-    PetalLength =< 2.35.
-iris(SepalLength, SepalWidth, PetalLength, PetalWidth, versicolor) :-
-    PetalWidth =< 1.75.
+iris(SepalLength, SepalWidth, PetalLength, PetalWidth, setosa) :- PetalLength =< 2.35.
+iris(SepalLength, SepalWidth, PetalLength, PetalWidth, versicolor) :- PetalWidth =< 1.75.
 iris(SepalLength, SepalWidth, PetalLength, PetalWidth, virginica).
